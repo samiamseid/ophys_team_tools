c3865020e755cae69c41a3fb1e6beb3a52ea8a8e
samiamseid <sams@alleninstitute.org>
2025-03-06 14:23:45 -0800

    package-ify
    adding package stuff for easy deploy

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ophys_team_tools',
+    version='1.0.0',
+    author='Sam Seid',
+    author_email='sams@alleninstitute.org',
+    description='Ophys Core team functions',
+    packages=find_packages()
+)
