3e32722ff11391a7bdb0216525a3b70084da4159
Sam Seid <sams@alleninstitute.org>
2025-07-10 10:24:02 -0700

    iterate version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ophys_team_tools',
-    version='1.3.6',
+    version='1.3.7',
     author='Sam Seid',
     author_email='sams@alleninstitute.org',
     description='Ophys Core team functions',
